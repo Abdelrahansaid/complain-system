eafc8d7cff83f811044a03b0342030321c481caf
Abdelrahman said <104476867+Abdelrahansaid@users.noreply.github.com>
2024-06-24 19:53:29 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -33,42 +33,42 @@ def process_arabic_text(text):
     Returns:
         The corrected Arabic sentence.
     """
-   #
+##
     
-    # Emoji removal (updated for compatibility)
+    # Emoji removal
     emoji_pattern = re.compile(pattern = "["
-                               u"\U0001F600-\U0001F64F"
-                               u"\U0001F300-\U0001F5FF"
-                               u"\U0001F680-\U0001F6FF"
-                               u"\U0001F1E0-\U0001F1FF"
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                                "]+", flags = re.UNICODE)
-    
     corrected_text = emoji_pattern.sub(r"", corrected_text)
 
     # Normalization
-    normalized_text = strip_tashkeel(corrected_text)
-    normalized_text = araby.normalize_hamza(normalized_text)
-    normalized_text = normalized_text.replace('ي', 'ى')
-    normalized_text = normalized_text.replace('ء','ا')
-    normalized_text = normalized_text.replace('ة', 'ه')
+    normalized_text = strip_tashkeel(corrected_text)  # Remove diacritics
+    normalized_text = araby.normalize_hamza(normalized_text)  # Normalize hamzas
+    normalized_text = normalized_text.replace('ي', 'ى')  # Alef maksura to ya
+    normalized_text = normalized_text.replace('ء','ا')  # Alef with hamza to alef
+    normalized_text = normalized_text.replace('ة', 'ه')  # Teh marbuta to ta marbuta
 
     # Punctuation Removal
-    punctuations = "!؟؛،«»\\,\\:\\;\\(\\)\\(//)\\-\\_\\~\\#\\@\\$\\%\\[\\]\\{\\}\\+\\|\\*\\=\\<\\>\\^\\&\\٪"
+    punctuations = "!؟؛،«»\\,\\:\\;\\(\\)\\(//)\\-\\_\\~\\#\\@\\$\\%\\[\\]\\{\\}\\+\\|\\*\\=\\<\\>\\^\\&\\٪"  # Arabic punctuation marks
     normalized_text = re.sub(f"[{punctuations}]", "", normalized_text)
 
     # Tokenization
-    tokens = simple_word_tokenize(normalized_text)
+    tokens = simple_word_tokenize(normalized_text)  # Split into words
 
     # Stop word removal
-    stop_words = set(stopwords.words("arabic"))
+    stop_words = set(stopwords.words("arabic"))  # Load Arabic stop words
     filtered_tokens = [token for token in tokens if token not in stop_words]
 
     # Stemming
-    stemmer = SnowballStemmer("arabic")
+    stemmer = SnowballStemmer("arabic")  # Use Arabic-specific stemmer
     stemmed_tokens = [stemmer.stem(token) for token in filtered_tokens]
     stemmed_text = ' '.join(stemmed_tokens).rstrip('.?!').lstrip('.?!')
 
     return stemmed_text
+
 
 # Read the xlsx file
 df = pd.read_excel('complains 2.xlsx')
